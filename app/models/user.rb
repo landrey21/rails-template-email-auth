062006bad270fb837431793b63d81bf12cacd354
Jason <landrey21@yahoo.com>
2014-11-10 15:20:19 -0600

    Removed some commented out code

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,23 +4,22 @@ class User < ActiveRecord::Base
 
   self.table_name = 'user'
 
-  #before_save :encrypt_password
-  #after_save :clear_password
-
   EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i 
   validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   validates :password, :confirmation => true
-  #validates_length_of :password, :in => 6..20, :on => :create
 
+  #---------------------------------------------------------
   def authenticate(input_password)
     self.password == BCrypt::Engine.hash_secret(input_password, self.salt)
   end
 
+  #---------------------------------------------------------
   def encrypt_password(password_str)
     self.salt = BCrypt::Engine.generate_salt
     self.password = BCrypt::Engine.hash_secret(password_str, self.salt)
   end
 
+  #---------------------------------------------------------
   def email_confirmation_hash(email_str)
     confirmation_salt = self.salt.nil? ? BCrypt::Engine.generate_salt : self.salt
     self.email_confirmation = Digest::MD5.hexdigest(confirmation_salt + email_str)
