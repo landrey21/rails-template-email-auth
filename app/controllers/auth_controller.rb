740079a246c48d1c121d82f412837c0f5ba39f22
Jason <landrey21@yahoo.com>
2014-11-12 09:09:27 -0600

    renamed db columns

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -17,24 +17,24 @@ class AuthController < ApplicationController
     if @user == nil
       @user = User.new
       flash.now[:alert] = 'Incorrect email or password.'
-    elsif !@user.delete_time.nil?
+    elsif !@user.deleted_at.nil?
       flash.now[:alert] = 'Inactive account.'
-    elsif @user.failed_logins > 5
-      flash.now[:alert] = 'This account has been locked due to excessive login attempts.'
+    elsif @user.failed_signins > 5
+      flash.now[:alert] = 'This account has been locked due to excessive sign in attempts.'
     elsif !@user.authenticate(params[:password])
       flash.now[:alert] = 'Incorrect email or password.'
-      @user.failed_logins += 1
+      @user.failed_signins += 1
       @user.save
     elsif @user.is_blocked
       flash.now[:alert] = 'This account has been blocked.'
     elsif @user.email_confirmation != '1'
-      flash.now[:alert] = 'This email address must be confirmed before you can login.  Please check your email.'
+      flash.now[:alert] = 'This email address must be confirmed before you can sign in.  Please check your email.'
     else
       session[:user_id] = @user.id
       session[:user_email] = params[:email]
       session[:user_name] = @user.name
-      @user.failed_logins = 0
-      @user.last_login = Time.now.to_s(:db)
+      @user.failed_signins = 0
+      @user.last_signin = Time.now.to_s(:db)
       @user.save
       redirect_to :app
       return
@@ -66,7 +66,7 @@ class AuthController < ApplicationController
       charset = 'ACDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz2345679'
       password_str = (0...8).map{ charset[rand(charset.size)] }.join
       @user.encrypt_password(password_str)
-      @user.failed_logins = 0
+      @user.failed_signins = 0
       if @user.save
         flash.now[:notice] = 'Your password has been reset.  Please check your email.'
         # send email with new password
